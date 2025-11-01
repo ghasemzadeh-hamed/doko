95cd5f5fc9fcc01b604a3ed18f5dd7d8c4abb124
Hamed Ghasemzadeh <ghasemzadeh.it@gmail.com>
2025-11-01 19:42:14 +0330

    Switch backend to FastAPI auto-generated API

--- dokoplatform/asgi.py
+++ dokoplatform/asgi.py
@@ -1,16 +1,8 @@
-"""
-ASGI config for dokoplatform project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
-"""
+"""ASGI configuration exposing the FastAPI application for the platform."""
 
 import os
 
-from django.core.asgi import get_asgi_application
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dokoplatform.settings")
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dokoplatform.settings')
+from fastapi_app import app as application  # noqa: E402  (imports after Django config)
 
-application = get_asgi_application()
---
+++ fastapi_app/__init__.py
@@ -0,0 +1,5 @@
+"""FastAPI application exposing the Doko data model."""
+
+from .main import app
+
+__all__ = ["app"]
